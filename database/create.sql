958b7ef3de7a5dbb758b194b90f157d412e6f294
bearb1213 <bearb1145@gmail.com>
2025-07-13 00:00:19 +0600

    Base de donne

---
+++ database/create.sql
@@ -0,0 +1,132 @@
+\c postgres
+DROP DATABASE IF EXISTS biblio ;
+CREATE DATABASE biblio;
+\c biblio
+
+CREATE TABLE adhesion_type(
+   id SERIAL,
+   type VARCHAR(50),
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE utilisateur_type(
+   id VARCHAR(50),
+   type VARCHAR(50),
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE auteur(
+   id VARCHAR(50),
+   nom VARCHAR(255),
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE genre(
+   id SERIAL,
+   genre VARCHAR(50),
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE livre(
+   id SERIAL,
+   titre VARCHAR(255),
+   synopsis TEXT,
+   date_premier_edition DATE,
+   langue VARCHAR(50),
+   nb_page INTEGER,
+   restriction INTEGER,
+   id_auteur VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_auteur) REFERENCES auteur(id)
+);
+
+CREATE TABLE livre_genre(
+   id SERIAL,
+   id_livre INTEGER NOT NULL,
+   id_genre INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_livre) REFERENCES livre(id),
+   FOREIGN KEY(id_genre) REFERENCES genre(id)
+);
+
+CREATE TABLE exemplaire(
+   id SERIAL,
+   date_in DATE,
+   id_livre INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_livre) REFERENCES livre(id)
+);
+
+CREATE TABLE utilisateur(
+   id SERIAL,
+   nom VARCHAR(255),
+   prenom VARCHAR(255),
+   email VARCHAR(255),
+   mdp VARCHAR(255),
+   date_naissance DATE,
+   date_in DATE,
+   id_type VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_type) REFERENCES utilisateur_type(id)
+);
+
+CREATE TABLE adhesion(
+   id SERIAL,
+   date_in DATE,
+   date_fin DATE,
+   id_utilisateur INTEGER NOT NULL,
+   id_type INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id),
+   FOREIGN KEY(id_type) REFERENCES adhesion_type(id)
+);
+
+CREATE TABLE reservation(
+   id SERIAL,
+   date_in TIMESTAMP,
+   motif TEXT,
+   id_utilisateur INTEGER NOT NULL,
+   id_exemplaire INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id),
+   FOREIGN KEY(id_exemplaire) REFERENCES exemplaire(id)
+);
+
+CREATE TABLE reservation_status(
+   id SERIAL,
+   statu VARCHAR(50),
+   date_in TIMESTAMP,
+   id_reservation INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_reservation) REFERENCES reservation(id)
+);
+
+CREATE TABLE pret(
+   id SERIAL,
+   date_in TIMESTAMP,
+   id_utilisateur INTEGER NOT NULL,
+   id_exemplaire INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id),
+   FOREIGN KEY(id_exemplaire) REFERENCES exemplaire(id)
+);
+
+CREATE TABLE pret_status(
+   id SERIAL,
+   statu VARCHAR(50),
+   date_in TIMESTAMP,
+   date_fin TIMESTAMP,
+   id_pret INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_pret) REFERENCES pret(id)
+);
+
+
+CREATE TABLE prolongement( 
+   id SERIAL,
+   statu VARCHAR(50),
+   date_in TIMESTAMP,
+   id_pret INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_pret) REFERENCES pret(id)
+);
